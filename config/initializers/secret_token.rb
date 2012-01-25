33e4433c8684515610562b4aac2afa97ba468921
Yutaka HARA <yutaka.hara.gmail.com>
2012-01-25 21:57:44 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyApi::Application.config.secret_token = 'b8a2216782756508ba0d5ae043ff37df052c46e82c8eabd9a05e6171732149745b3392f66615766e9fbfe3eb9a9f1f08ed0c96c846c5eaca528c6865df4985e9'

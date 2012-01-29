1f0644179e28714ccb45095a6bd6124aa4b2e636
Yutaka HARA <yutaka.hara.gmail.com>
2012-01-29 18:08:11 +0900

    use slim

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module RubyApi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use SASS instead of SCSS
+    config.sass.preferred_syntax = :sass
   end
 end

f44d83d7981df0db369c2c587c84fdfb0cdd7d33
Yutaka HARA <yutaka.hara.gmail.com>
2012-02-16 22:54:37 +0900

    fix: current language should be selected on language menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def language_options
+    pairs = Language.all.map{|l| [l.english_name, l.short_name]}
+    Hash[*pairs.flatten]
+  end
+
+  def current_language
+    current = @language || Language.default
+    current.short_name
+  end
 end

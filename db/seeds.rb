da4e96cb569638ecb246a0b7188964dc160c8fc2
Yutaka HARA <yutaka.hara.gmail.com>
2012-03-04 18:04:40 +0900

    Create Language['cp'] in setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 # Create Languages
-
 [
   ["en", "English", "English"],
   ["ja", "Japanese", "日本語"],
+  ["cp", "Capitalized", "CAPITALIZED"], # for experimental use
 ].each do |s, e, n|
   Language.create(short_name: s, english_name: e, native_name: n)
 end

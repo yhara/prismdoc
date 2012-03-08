0383d649bdba82bd7515ab2832f54bb44e87b216
Yutaka HARA <yutaka.hara.gmail.com>
2012-03-08 20:31:39 +0900

    fix: LibraryEntry#path returns wrong path for _builtin

--- app/models/library_entry.rb
+++ app/models/library_entry.rb
@@ -1,11 +1,13 @@
 class LibraryEntry < Entry
   has_many :modules, class_name: "ModuleEntry", foreign_key: "library_id"
   def path(language)
-    return "/" if self.name == "_builtin"
-
     [ 
       "/", Rack::Utils.escape(language.short_name),
-      "/", Rack::Utils.escape(self.fullname)
+      (if self.name == "_builtin"
+         ""
+       else
+         "/" + Rack::Utils.escape(self.fullname)
+       end)
     ].join
   end
 end

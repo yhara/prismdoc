5c2c932d2e056d4603ad6bcfd0ab5339dead055c
Yutaka HARA <yutaka.hara.gmail.com>
2012-03-09 14:58:51 +0900

    new: belong_name (eg. 'Array#map' for '_builtin;Array#map')

--- app/models/constant_entry.rb
+++ app/models/constant_entry.rb
@@ -2,6 +2,10 @@ class ConstantEntry < Entry
   belongs_to :library, class_name: "LibraryEntry"
   belongs_to :module, class_name: "ModuleEntry"
 
+  def belong_name
+    self.module.name + "::" + self.name
+  end
+
   def path(language)
     self.module.path(language) +
       "/" + "::" + Rack::Utils.escape(self.name)
--- app/models/instance_method_entry.rb
+++ app/models/instance_method_entry.rb
@@ -1,4 +1,8 @@
 class InstanceMethodEntry < MethodEntry
+  def belong_name
+    self.module.name + "#" + self.name
+  end
+
   def path(language)
     self.module.path(language) +
       "/" + Rack::Utils.escape(self.name)

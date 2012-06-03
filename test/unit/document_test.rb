13d70f8d6c6848b1b9dbe0bb2c3afa1e89ef7682
Yutaka HARA <yutaka.hara.gmail.com>
2012-06-04 01:42:48 +0900

    added test for Document#translated

--- test/unit/document_test.rb
+++ test/unit/document_test.rb
@@ -23,15 +23,6 @@ module RubyApi
         end
       end
 
-#      context "and setting #translated" do
-#        should "set 'no' for document not translated" do
-#          doc = Document.create(entry: entries("object19"),
-#                                language: languages("ja"),
-#                                body: "",
-#          assert_equal "no", doc.translated
-#        end
-#      end
-      
       context "and creating paragraphs" do
         setup do
           @eng_doc = Document.create(entry: entries("builtin19"),
@@ -52,6 +43,45 @@ module RubyApi
           paras = doc.paragraphs
           assert_equal @eng_doc.paragraphs, paras.map(&:original)
           assert_equal [nil, nil, nil], paras.map(&:body)
+        end
+      end
+
+      context "and setting #translated" do
+        setup do
+          @eng_doc18 = Document.create!(entry: entries("builtin18"),
+                                        language: languages("en"),
+                                        body: "aa\nbb\n\n\ncc\n\ncc\n")
+          @eng_doc19 = Document.create!(entry: entries("builtin19"),
+                                        language: languages("en"),
+                                        body: "aa\nbb\n\n\ncc\n\ncc\n")
+        end
+
+        should "set 'no' for a document not translated at all" do
+          doc = Document.create!(entry: entries("builtin19"),
+                                 language: languages("tt"))
+          assert_equal "no", doc.translated
+        end
+
+        should "set 'partially' for a document partially translated" do
+          doc18 = Document.create!(entry: entries("builtin18"),
+                                   language: languages("tt"))
+          doc18.paragraphs.first.update_attributes!(body: "ddd")
+          
+          doc = Document.create!(entry: entries("builtin19"),
+                                 language: languages("tt"))
+          assert_equal "partially", doc.translated
+        end
+
+        should "set 'yes' for a document fully translated" do
+          doc18 = Document.create!(entry: entries("builtin18"),
+                                   language: languages("tt"))
+          doc18.paragraphs.each do |para18|
+            para18.update_attributes!(body: "ddd")
+          end
+          
+          doc = Document.create!(entry: entries("builtin19"),
+                                 language: languages("tt"))
+          assert_equal "yes", doc.translated
         end
       end
     end

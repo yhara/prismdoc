445826d069307ff047676661734fda0c8c40f3e3
Yutaka HARA <yutaka.hara.gmail.com>
2012-06-04 02:14:25 +0900

    fix: TopController failed when HTTP_ACCEPT_LANGUAGE is not given

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,13 @@
 #
 class TopController < ApplicationController
   def index
-    given = request.env['HTTP_ACCEPT_LANGUAGE'][/^[a-z]{2}/]
-    language = Language.find_by_short_name(given) || Language.default
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+    if accept_language
+      given = accept_language[/^[a-z]{2}/]
+      language = Language.find_by_short_name(given) || Language.default
+    else
+      language = Language.default
+    end
 
     redirect_to "/#{language.short_name}/"
   end
--- test/functional/top_controller_test.rb
+++ test/functional/top_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TopControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_redirected_to "/en/"
   end
 
 end

0df6bfa30fc8244ce3ad1bebad15a5ff62af887e
Yutaka HARA <yutaka.hara.gmail.com>
2012-06-03 23:23:52 +0900

    Added some test for Document

---
+++ test/unit/document_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+require 'rdoc_helper.rb'
+
+module RubyApi
+  class DocumentTest < ActiveSupport::TestCase
+    context "when creating an instance" do
+      should "check <translated> has valid value" do
+        doc = Document.new(entry: entries("builtin19"),
+                           language: languages("ja"),
+                           body: "",
+                           version: versions("v193"),
+                           translated: "xxxx")
+        assert_equal false, doc.valid?
+        assert_match /unknown state/, doc.errors[:translated].first
+      end
+
+      should "check it is not translated yet" do
+        doc = Document.new(entry: entries("object19"),
+                           language: languages("ja"),
+                           body: "",
+                           version: versions("v193"))
+        assert_equal false, doc.valid?
+        assert_match /already/, doc.errors[:entry_id].first
+      end
+
+#      context "and setting #translated" do
+#        should "set 'no' for document not translated" do
+#          doc = Document.create(entry: entries("object19"),
+#                                language: languages("ja"),
+#                                body: "",
+#                                version: versions("v193"))
+#          assert_equal "no", doc.translated
+#        end
+#      end
+    end
+  end
+end

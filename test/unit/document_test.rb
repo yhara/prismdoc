31c37d2293f99a1987347bfdc4ecaef6a4b5102b
Yutaka HARA <yutaka.hara.gmail.com>
2012-06-04 01:28:15 +0900

    refactored test

--- test/unit/document_test.rb
+++ test/unit/document_test.rb
@@ -4,21 +4,23 @@ require 'rdoc_helper.rb'
 module RubyApi
   class DocumentTest < ActiveSupport::TestCase
     context "when creating an instance" do
-      should "check <translated> has valid value" do
-        doc = Document.new(entry: entries("builtin19"),
-                           language: languages("ja"),
-                           body: "")
-        doc.translated = "xxxx"
-        assert_equal false, doc.valid?
-        assert_match /unknown state/, doc.errors[:translated].first
-      end
+      context "and validating attirbutes" do
+        should "check <translated> has valid value" do
+          doc = Document.new(entry: entries("builtin19"),
+                             language: languages("ja"),
+                             body: "")
+          doc.translated = "xxxx"
+          assert_equal false, doc.valid?
+          assert_match /unknown state/, doc.errors[:translated].first
+        end
 
-      should "check it is not translated yet" do
-        doc = Document.new(entry: entries("object19"),
-                           language: languages("ja"),
-                           body: "")
-        assert_equal false, doc.valid?
-        assert_match /already/, doc.errors[:entry_id].first
+        should "check it is not translated yet" do
+          doc = Document.new(entry: entries("object19"),
+                             language: languages("ja"),
+                             body: "")
+          assert_equal false, doc.valid?
+          assert_match /already/, doc.errors[:entry_id].first
+        end
       end
 
 #      context "and setting #translated" do
@@ -30,25 +32,27 @@ module RubyApi
 #        end
 #      end
       
-      should "create paragraphs for English document" do
-        doc = Document.create(entry: entries("builtin19"),
-                              language: languages("en"),
-                              body: "aa\nbb\n\n\ncc\n\ncc\n")
-        paras = doc.paragraphs
-        assert_equal [nil, nil, nil], paras.map(&:original)
-        assert_equal ["aa\nbb", "cc", "cc"], paras.map(&:body)
-        assert paras[1].id == paras[2].id
-      end
-      
-      should "create paragraphs for translated document" do
-        orig = Document.create(entry: entries("builtin19"),
-                               language: languages("en"),
-                               body: "aa\nbb\n\n\ncc\n\ncc\n")
-        doc = Document.create(entry: entries("builtin19"),
-                              language: languages("tt"))
-        paras = doc.paragraphs
-        assert_equal orig.paragraphs, paras.map(&:original)
-        assert_equal [nil, nil, nil], paras.map(&:body)
+      context "and creating paragraphs" do
+        setup do
+          @eng_doc = Document.create(entry: entries("builtin19"),
+                                     language: languages("en"),
+                                     body: "aa\nbb\n\n\ncc\n\ncc\n")
+        end
+
+        should "create paragraphs for English document" do
+          paras = @eng_doc.paragraphs
+          assert_equal [nil, nil, nil], paras.map(&:original)
+          assert_equal ["aa\nbb", "cc", "cc"], paras.map(&:body)
+          assert paras[1].id == paras[2].id
+        end
+        
+        should "create paragraphs for translated document" do
+          doc = Document.create(entry: entries("builtin19"),
+                                language: languages("tt"))
+          paras = doc.paragraphs
+          assert_equal @eng_doc.paragraphs, paras.map(&:original)
+          assert_equal [nil, nil, nil], paras.map(&:body)
+        end
       end
     end
   end
